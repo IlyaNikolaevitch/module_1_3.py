2415959bfc1050963138f0446ed8732630db319c
IlyaNikolaevitch <mister.bin1981@gmail.com>
2024-09-18 21:42:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ name = 'Ilya'
 print('Name:',name)
 age = 43
 print('Age:',age)
-age = 45
-print('New Age:',age)
+age = (age+1)
+new_age = 44
+print('New age:', age) # is_student= age<age+1
+is_student = new_age == age
+print('Is_student:', is_student), age == (age+1)
